c30c8c0932224a6e526b1a7eff6019ba392af8ce
doodleigit <doodleigit@doodleinc.net>
2019-12-10 10:50:24 +0600

    commit 12/10/2019(Group Design App)

--- app/src/main/java/com/liker/android/Group/CreateGroupActivity.java
+++ app/src/main/java/com/liker/android/Group/CreateGroupActivity.java
@@ -2,14 +2,31 @@ package com.liker.android.Group;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.liker.android.R;
 
-public class CreateGroupActivity extends AppCompatActivity {
+public class CreateGroupActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ImageView imageCancelCreateGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_group);
+        imageCancelCreateGroup = findViewById(R.id.imageCancelCreateGroup);
+        imageCancelCreateGroup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.imageCancelCreateGroup:
+                finish();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/liker/android/Group/model/App.java
@@ -0,0 +1,13 @@
+package com.liker.android.Group.model;
+
+public class App {
+
+    public String name;
+    public int drawable;
+
+    public App(String name, int drawable) {
+        this.name = name;
+        this.drawable = drawable;
+    }
+
+}
